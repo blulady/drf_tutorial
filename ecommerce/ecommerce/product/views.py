5ae352456a661a367001fed480a0e153cb249d59
blulady <blulady@gmail.com>
2023-08-13 11:22:04 -0700

    commiting ignore

--- ecommerce/ecommerce/product/views.py
+++ ecommerce/ecommerce/product/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
+from .models import Category
+from .serializers import CategorySerializer
+
 
 # Create your views here.
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    queryset = Category.objects.all()
+
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
